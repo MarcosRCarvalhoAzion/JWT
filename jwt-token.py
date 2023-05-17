951d76aa4d7ffa6e3d61cbd370443ce97cd66983
Marcos R Carvalho <marcosrcarvalho42@gmail.com>
2023-05-17 19:56:03 -0300

    modificacao para trabalhar com argumentos

--- jwt-token.py
+++ jwt-token.py
@@ -5,33 +5,13 @@ Script para Gerar um Token JWT a partir de uma lista de Key IDs.
 import jwt
 import os
 import random
+import sys
 import time
 
 os.system('clear')
-# Comandos para criar CHAVE e SECRET para teste
-# OBS: A Chave e o Secret devem ser iguais aos do Json Args da Function JWT
-
-# OSX:
-#  echo -n 'qualquer_string_aqui...' | md5 -r | tr '[:lower:]' '[:upper:]'
-
-# Linux:
-#  echo -n 'qualquer_string_aqui...' | md5sum | tr '[:lower:]' '[:upper:]'
-
-# Exemplo: 
-
-#  echo -n 'chave_pic_pay_2023' | md5 -r | tr '[:lower:]' '[:upper:]'
-#  echo -n 'secret_pic_pay_2023' | md5 -r | tr '[:lower:]' '[:upper:]'
-
-##########################################################################
-
-# formato do dic 
-# CHAVE_MD5 : SECRET_MD5
-
-# chave_pic_pay_2023 (string) = 0AC4C3DEDB9B9263BEC8FF929D750D8A (MD5)
-# secret_pic_pay_2023 (string) = 92E7D28F180A5BFC12139B1EA76FA897 (MD5)
 
 kids_and_secrets = {
-    '0AC4C3DEDB9B9263BEC8FF929D750D8A' : '92E7D28F180A5BFC12139B1EA76FA897'
+    f'{sys.argv[1]}' : f'{sys.argv[2]}'
 }
 
 # get a random kid with secret
